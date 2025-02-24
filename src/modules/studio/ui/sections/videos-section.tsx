f9ac4270cf3ea45f0be5255991c597f30de031a6
Jayprajapati19 <jayp37679@gmail.com>
2025-02-24 20:15:50 +0530

    video section

---
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { trpc } from "@/trpc/client"
+
+export const VideosSection = () => {
+
+    const [data] = trpc.studio.getMany.useSuspenseQuery
+
+    // 5:40
+
+    return (
+        <div>
+            Videos sections
+        </div>
+    )
+}
