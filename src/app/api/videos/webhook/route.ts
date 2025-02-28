ee41751e31d05a2e038154cdaf56c4c37fecc6a9
Jayprajapati19 <jayp37679@gmail.com>
2025-02-28 12:33:21 +0530

    Enhance video management by adding support for video deletion, updating video details, and integrating thumbnail and preview URLs. Introduce video visibility options and format duration for better display in the UI.

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -2,9 +2,11 @@ import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import {
     VideoAssetCreatedWebhookEvent,
+    VideoAssetDeletedWebhookEvent,
     VideoAssetErroredWebhookEvent,
     VideoAssetReadyWebhookEvent,
-    VideoAssetTrackReadyWebhookEvent,
+    VideoAssetTrackReadyWebhookEvent
+    ,
 
 } from "@mux/mux-node/resources/webhooks"
 import { mux } from "@/lib/mux";
@@ -17,7 +19,8 @@ type WebhookEvent =
     | VideoAssetCreatedWebhookEvent
     | VideoAssetErroredWebhookEvent
     | VideoAssetReadyWebhookEvent
-    | VideoAssetTrackReadyWebhookEvent;
+    | VideoAssetTrackReadyWebhookEvent
+    | VideoAssetDeletedWebhookEvent;
 
 
 
@@ -51,6 +54,10 @@ export const POST = async (request: Request) => {
             if (!data.upload_id) {
                 return new Response("No upload id found", { status: 400 });
             }
+
+            console.log("creating video: ", { uploadId: data.upload_id });
+
+
             await db
                 .update(videos)
                 .set({
@@ -73,7 +80,11 @@ export const POST = async (request: Request) => {
                 return new Response("Missig playback ID", { status: 400 });
             }
 
-            const thumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.jpg`
+            const thumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.jpg`;
+            const previewUrl = `https://image.mux.com/${playbackId}/animated.gif`;
+
+
+            const duration = data.duration ? Math.round(data.duration * 1000) : 0;
 
             await db
                 .update(videos)
@@ -82,8 +93,72 @@ export const POST = async (request: Request) => {
                     muxPlaybackId: playbackId,
                     muxAssetId: data.id,
                     thumbnailUrl,
+                    previewUrl,
+                    duration,
+
+
                 })
                 .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+
+        case "video.asset.errored": {
+            const data = payload.data as VideoAssetErroredWebhookEvent["data"];
+
+            if (!data.upload_id) {
+                return new Response("Missig upload ID", { status: 400 });
+            }
+
+            await db
+                .update(videos)
+                .set({
+                    muxStatus: data.status,
+                })
+                .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+
+        case "video.asset.deleted": {
+            const data = payload.data as VideoAssetDeletedWebhookEvent["data"];
+
+            if (!data.upload_id) {
+                return new Response("Missig upload ID", { status: 400 });
+            }
+
+
+            console.log("Deleting video: ", { uploadId: data.upload_id });
+
+
+            await db
+                .delete(videos)
+                .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+
+        case "video.asset.track.ready": {
+            const data = payload.data as VideoAssetTrackReadyWebhookEvent["data"] & {
+                asset_id: string;
+            }
+
+            console.log("Track ready ");
+
+            // Typescript incorrectly says that asset_id does not exist
+            const assetId = data.asset_id;
+            const trackId = data.id;
+            const status = data.status;
+
+
+            if (!assetId) {
+                return new Response("Missig asset ID", { status: 400 });
+            }
+
+            await db
+                .update(videos)
+                .set({
+                    muxTrackId: trackId,
+                    muxTrackStatus: status,
+                })
+                .where(eq(videos.muxAssetId, assetId));
             break;
         }
     }
--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -2,7 +2,7 @@
 
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-
+import { format } from "date-fns";
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client"
 import { InfiniteScroll } from "@/components/infinite-scroll";
@@ -11,6 +11,8 @@ import { InfiniteScroll } from "@/components/infinite-scroll";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import Link from "next/link";
 import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
+import { snakeCaseToTitle } from "@/lib/utils";
+import { Globe2Icon, LockIcon } from "lucide-react";
 
 
 export const VideosSection = () => {
@@ -53,18 +55,36 @@ const VideosSectionSuspense = () => {
                                     <TableCell>
                                         <div className="flex items-center gap-4 ">
                                             <div className="relative aspect-video w-36 shrink-0  ">
-                                                <VideoThumbnail />
+                                                <VideoThumbnail
+                                                    imageUrl={video.thumbnailUrl}
+                                                    previewUrl={video.previewUrl}
+                                                    title={video.title}
+                                                    duration={video.duration || 0}
+                                                />
+                                            </div>
+                                            <div className="flex flex-col overflow-hidden gap-y-1  ">
+                                                <span className="text-sm line-clamp-1"> {video.title}</span>
+                                                <span className="text-xs text-muted-foreground line-clamp-1"> {video.description || "No description"}</span>
                                             </div>
                                         </div>
                                     </TableCell>
                                     <TableCell>
-                                        visibility
+                                        <div className="flex items-center">
+                                            {video.visibility === "private" ? (
+                                                <LockIcon className="size-4 mr-2" />
+                                            ) : (
+                                                <Globe2Icon className="size-4 mr-2" />
+                                            )}
+                                            {snakeCaseToTitle(video.visibility)}
+                                        </div>
                                     </TableCell>
                                     <TableCell>
-                                        status
+                                        <div className="flex items-center ">
+                                            {snakeCaseToTitle(video.muxStatus || "error")}
+                                        </div>
                                     </TableCell>
-                                    <TableCell>
-                                        date
+                                    <TableCell className="text-sm truncate ">
+                                        {format(new Date(video.createdAt), "d MMM yyyy")}
                                     </TableCell>
                                     <TableCell>
                                         views
