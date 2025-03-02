194ca5dbaac7b9d162fb491ab1904d83b1b0a140
Jayprajapati19 <jayp37679@gmail.com>
2025-03-02 07:13:58 +0530

    Add MuxPlayer integration and enhance video upload modal: implement video player component and improve success handling in upload modal

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -6,12 +6,11 @@ import { StudioUploader } from "./studio-uploader"
 import { Button } from "@/components/ui/button"
 import { ResponsiveModal } from "@/components/responsive-modal"
 import { Loader2Icon, PlusIcon } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 export const StudioUploadModal = () => {
-
+    const router = useRouter();
     const utils = trpc.useUtils();
-
-
     const create = trpc.videos.create.useMutation({
         onSuccess: () => {
             toast.success("Video created successfully ✅")
@@ -24,8 +23,16 @@ export const StudioUploadModal = () => {
 
     });
 
-    return (
+    const onSuccess = () => {
+        if (!create.data?.video.id) return;
 
+        create.reset();
+        router.push(`/studio/videos/${create.data.video.id}`);
+
+    }
+
+
+    return (
         <>
             <ResponsiveModal
                 title="Upload video"
@@ -33,7 +40,7 @@ export const StudioUploadModal = () => {
                 onOpenChange={() => create.reset()}
             >
                 {create.data?.url
-                    ? < StudioUploader endpoint={create.data.url} onSuccess={() => { }} />
+                    ? < StudioUploader endpoint={create.data.url} onSuccess={onSuccess} />
                     : <Loader2Icon />
                 }
             </ResponsiveModal>
--- src/modules/videos/ui/components/video-player.tsx
+++ src/modules/videos/ui/components/video-player.tsx
@@ -1,3 +1,7 @@
+"use client"
+
+import MuxPlayer from "@mux/mux-player-react"
+
 interface VideoPlayerProps {
     playbackId?: string | null | undefined;
     thumbnailUrl?: string | null | undefined;
@@ -5,4 +9,27 @@ interface VideoPlayerProps {
     onPlay?: () => void;
 };
 
-export const VideoPlayer = () => { }
+export const VideoPlayer = ({
+    playbackId,
+    thumbnailUrl,
+    autoPlay,
+    onPlay,
+
+
+}: VideoPlayerProps) => {
+    // if (!playbackId) return null;
+
+    return (
+        <MuxPlayer
+            playbackId={playbackId || ""}
+            poster={thumbnailUrl || "/placeholder.svg"}
+            playerInitTime={0}
+            autoPlay={autoPlay}
+            thumbnailTime={0}
+            className="w-full h-full object-contain"
+            accentColor="#FF2056"
+            onPlay={onPlay}
+
+        />
+    );
+};
