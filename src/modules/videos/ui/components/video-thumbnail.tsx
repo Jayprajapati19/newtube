7707b36730e74919c4c1c5350499de0eb8aaf473
Jayprajapati19 <jayp37679@gmail.com>
2025-03-02 09:06:06 +0530

    Add upload functionality and thumbnail handling: implement upload routes, constants for fallback thumbnails, and integrate thumbnail display in form section

--- src/modules/videos/ui/components/video-thumbnail.tsx
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -1,5 +1,6 @@
 import { formateDuration } from "@/lib/utils";
 import Image from "next/image"
+import { THUMBNAIL_FALLBACK } from "../../constants";
 
 interface VideoThumbnailProps {
     title: string;
@@ -20,7 +21,7 @@ export const VideoThumbnail = ({
             {/* Thumbnail wrapper  */}
             <div className="relative w-full overflow-hidden  rounded-xl aspect-video ">
                 <Image
-                    src={imageUrl ?? "/placeholder.svg"}
+                    src={imageUrl || THUMBNAIL_FALLBACK}
                     alt={title}
                     fill
                     className="h-full w-full object-cover group-hover:opacity-0"
@@ -28,7 +29,7 @@ export const VideoThumbnail = ({
                 />
                 <Image
                     unoptimized={!!previewUrl}
-                    src={previewUrl ?? "/placeholder.svg"}
+                    src={previewUrl || THUMBNAIL_FALLBACK}
                     alt={title}
                     fill
                     className="h-full w-full object-cover opacity-0 group-hover:opacity-100"
