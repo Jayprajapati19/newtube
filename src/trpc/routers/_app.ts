54f3462a4cba5cd825783a49adcdf7d3baae264f
Jayprajapati19 <jayp37679@gmail.com>
2025-06-02 13:24:07 +0530

    new

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,7 @@
 import { studioRouter } from '@/modules/studio/server/procedures';
 import { createTRPCRouter } from '../init';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
-import { videosRouter } from '@/modules/videos/server/procedures';
+import { videosRouter } from '@/modules/videos/ui/server/procedures';
 import { videoViewsRouter } from '@/modules/video-views/server/procedures';
 
 
