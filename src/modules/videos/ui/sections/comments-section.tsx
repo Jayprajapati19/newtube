d95e9075f34a1f7c987110668cbbe0b563d89694
Jayprajapati19 <jayp37679@gmail.com>
2025-06-04 19:12:28 +0530

    Implement comment deletion functionality and enhance comments section with loading state

--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -7,6 +7,7 @@ import { CommnetForm } from "@/modules/comments/ui/components/comment-form";
 import { CommentItem } from "@/modules/comments/ui/components/comment-item";
 import { DEFAULT_LIMIT } from "@/constants";
 import { InfiniteScroll } from "@/components/infinite-scroll";
+import { Loader2Icon } from "lucide-react";
 
 interface CommentsSectionsProps {
     videoId: string;
@@ -15,11 +16,21 @@ interface CommentsSectionsProps {
 
 export const CommetsSection = ({ videoId }: CommentsSectionsProps) => {
     return (
-        <Suspense fallback={<p>Loading....</p>}>
+        <Suspense fallback={<CommentsSectionSkeleton />}>
             <ErrorBoundary fallback={<p>Error</p>}>
                 <CommentsSectionSuspense videoId={videoId} />
             </ErrorBoundary>
         </Suspense>
+    )
+}
+
+const CommentsSectionSkeleton = () => {
+    return (
+        <div className="mt-6 flex justify-center items-center">
+            <Loader2Icon
+                className="textmuted-foreground size-7 animate-spin"
+            />
+        </div>
     )
 }
 
