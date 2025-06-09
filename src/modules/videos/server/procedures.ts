71247386688da728494205e08565869c1f76f186
Jayprajapati19 <jayp37679@gmail.com>
2025-06-09 18:03:22 +0530

    Add video section to home view; implement infinite scroll and error handling for video display

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -3,12 +3,82 @@ import { z } from "zod"
 import { subscriptions, users, videoReactions, videos, videoUpdateSchema, videoViews } from "@/db/schema";
 import { mux } from "@/lib/mux";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { and, eq, getTableColumns, inArray, isNotNull } from "drizzle-orm";
+import { and, desc, eq, getTableColumns, inArray, isNotNull, lt, or } from "drizzle-orm";
 import { TRPCError } from "@trpc/server";
 import { UTApi } from "uploadthing/server";
 import { workflow } from "@/lib/workflow";
 
 export const videosRouter = createTRPCRouter({
+    getMany: baseProcedure
+        .input(
+            z.object({
+                categoryId: z.string().uuid().nullish(),
+                cursor: z.object({
+                    id: z.string().uuid(),
+                    updatedAt: z.date(),
+                })
+                    .nullish(),
+                limit: z.number().min(1).max(100),
+
+            }),
+        )
+        .query(async ({ input }) => {
+            const { cursor, limit, categoryId } = input;
+
+            const data = await db
+                .select(
+                    {
+                        ...getTableColumns(videos),
+                        user: users,
+                        viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+                        likeCount: db.$count(videoReactions, and(
+                            eq(videoReactions.videoId, videos.id),
+                            eq(videoReactions.type, "like"),
+                        )),
+                        dislikeCount: db.$count(videoReactions, and(
+                            eq(videoReactions.videoId, videos.id),
+                            eq(videoReactions.type, "dislike"),
+                        )),
+                    }
+                )
+                .from(videos)
+                .innerJoin(users, eq(videos.userId, users.id))
+                .where(and(
+                    eq(videos.visibility, "public"),
+                    categoryId
+                        ? eq(videos.categoryId, categoryId)
+                        : undefined,
+                    cursor
+                        ? or(
+                            lt(videos.updatedAt, cursor.updatedAt),
+                            and(
+                                eq(videos.updatedAt, cursor.updatedAt),
+                                lt(videos.id, cursor.id)
+                            )
+                        )
+                        : undefined,
+                )).orderBy(desc(videos.updatedAt), desc(videos.id))
+                // add 1 to the limit to check if there is more data
+                .limit(limit + 1)
+
+            const hasMore = data.length > limit;
+            // Remove the last item if there is more data   
+            const items = hasMore ? data.slice(0, -1) : data;
+            // set rhe next cursor to the last item if there is more data
+            const lastItem = items[items.length - 1];
+
+            const nextCursor = hasMore
+                ? {
+                    id: lastItem.id,
+                    updatedAt: lastItem.updatedAt,
+                }
+                : null;
+
+            return {
+                items,
+                nextCursor,
+            };
+        }),
 
     getOne: baseProcedure
         .input(z.object({ id: z.string().uuid() }))
