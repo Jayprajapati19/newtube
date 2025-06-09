0afd2cd346a8a625026a564897db621607d5d227
Jayprajapati19 <jayp37679@gmail.com>
2025-06-09 17:25:21 +0530

    Add dynamic export to layout and page components; enhance search input and results section with suspense and error boundary handling

--- src/modules/home/ui/components/home-navbar/search-input.tsx
+++ src/modules/home/ui/components/home-navbar/search-input.tsx
@@ -3,14 +3,18 @@
 import { Button } from "@/components/ui/button";
 import { APP_URL } from "@/constants";
 import { SearchIcon, XIcon } from "lucide-react"
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 
 export const SearchInput = () => {
 
     const router = useRouter();
+    const searchParams = useSearchParams();
+    const query = searchParams.get("query") || "";
 
-    const [value, setValue] = useState("");
+    const categoryId = searchParams.get("categoryId") || "";
+
+    const [value, setValue] = useState(query);
 
     const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -19,6 +23,10 @@ export const SearchInput = () => {
         const newQuery = value.trim();
 
         url.searchParams.set("query", encodeURIComponent(newQuery));
+
+        if (categoryId) {
+            url.searchParams.set("categoryId", categoryId);
+        }
 
         if (newQuery === "") {
             url.searchParams.delete("query");
