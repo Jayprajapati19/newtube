741d15f4789d6f5abe711bb18a5ccf6c50bd94de
Jayprajapati19 <jayp37679@gmail.com>
2025-06-11 11:15:04 +0530

    Add user management features including user retrieval, UI components, and banner support

---
+++ src/modules/users/ui/sections/user-section.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+
+import { trpc } from "@/trpc/client";
+import { UserPageBanner } from "../components/user-page-banner";
+
+interface UserSectionProps {
+    userId: string;
+}
+
+export const UserSection = (props: UserSectionProps) => {
+    return (
+        <Suspense fallback={<p>Loadingg...</p>}>
+            <ErrorBoundary fallback={<p>Error.</p>}>
+                <UserSectionSuspense {...props} />
+            </ErrorBoundary>
+        </Suspense>
+
+    )
+}
+
+
+const UserSectionSuspense = ({ userId }: UserSectionProps) => {
+
+    const [user] = trpc.users.getOne.useSuspenseQuery({ id: userId });
+
+
+    return (
+        <div className="flex flex-col ">
+            <UserPageBanner user={user} />
+        </div>
+    );
+};
